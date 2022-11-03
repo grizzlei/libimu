33e65ca37d9160fe5da74f5ffd7fb3b5e8aaaad0
Hasan <hk@hasankaraman.dev>
2022-11-03 16:01:23 +0300

    fix: Rotation of imu in GL screen fixed.

--- src/demo.c
+++ src/demo.c
@@ -1,5 +1,29 @@
 #include <math.h>
 #include <stdio.h>
+/**
+ * MIT License
+ * Copyright (c) 2022 Hasan Karaman (github.com/grizzlei)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
@@ -46,10 +70,6 @@ void rotate_camera();
 void runner_serial(void * arg);
 
 
-////////////////////////////////////////////
-/// EXTERN INITIALIZATIONS
-
-
 int main(int argc, char *argv[])
 {
 	////////////////////////////////////////////
@@ -63,7 +83,7 @@ int main(int argc, char *argv[])
 
 	imu = imu_init();
 	imu_set_calibration_mode(&imu, IMU_CALIBMODE_ONCE);
-	// mpu6050 is initialized with ±1000 °/s gyro, ±4g accelerometer configurations
+	// mpu6050 is initialized with ±500 °/s gyro, ±4g accelerometer configurations
 	imu_set_gyro_scale_factor(&imu, 2.f/131.f);
 	imu_set_accelerometer_scale_factor(&imu, 2.f/16384.f);
 
@@ -135,7 +155,7 @@ int main(int argc, char *argv[])
 
 			// Tate-Bryant rotation sequence
 			glRotatef(r2d(eul.yaw), 0, 1, 0);
-			glRotatef(r2d(eul.pitch), 0, 0, 1);
+			glRotatef(-r2d(eul.pitch), 0, 0, 1);
 			glRotatef(r2d(eul.roll), 1, 0, 0);
 			
 			draw_axes(1.0, 1.0, 1.0);
